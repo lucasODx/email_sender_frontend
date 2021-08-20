0e7840e02449f43c448411f7ae5f8b1cb688d95d
Lucas <lucas.o.damacena@gmail.com>
2021-08-20 11:22:26 -0300

    Email sending with bootstrap

--- src/components/SendMail.vue
+++ src/components/SendMail.vue
@@ -1,18 +1,54 @@
 <template>
   <div>
-    <h1>Easy E-Mail Sender</h1>
+    <h1
+      style="
+        text-align: center;
+        margin: 50px;
+        color: white;
+        font-size: 50px;
+        font-family: 'Pacifico', sans-serif;
+      "
+    >
+      Easy E-Mail Sender
+    </h1>
   </div>
-  <div>
+  <div class="container">
     <form @submit="send">
-      <input type="text" placeholder="Seu nome" v-model="mail.name" />
-      <input
-        type="email"
-        placeholder="E-mail de destino"
-        v-model="mail.email"
-      />
-      <input type="text" placeholder="Assunto" v-model="mail.title" />
-      <textarea type="text" placeholder="Mensagem" v-model="mail.subject" />
-      <button>Enviar</button>
+      <div>
+        <input
+          class="form-control"
+          type="text"
+          placeholder="Seu nome"
+          v-model="mail.name"
+        />
+      </div>
+      <div>
+        <input
+          class="form-control"
+          type="email"
+          placeholder="E-mail de destino"
+          v-model="mail.email"
+        />
+      </div>
+      <div>
+        <input
+          class="form-control"
+          type="text"
+          placeholder="Assunto"
+          v-model="mail.title"
+        />
+      </div>
+      <div>
+        <textarea
+          class="form-control"
+          type="text"
+          placeholder="Mensagem"
+          v-model="mail.subject"
+        />
+      </div>
+      <div style="text-align:center; margin-top:20px">
+        <button class="btn btn-primary">Enviar</button>
+      </div>
     </form>
   </div>
 </template>
@@ -35,8 +71,14 @@ export default {
   methods: {
     send(e) {
       Mail.sendMail(this.mail).then((response) => {
-        console.log(response)
-      })
+        console.log(response);
+      });
+      this.mail = {
+        name: this.mail.name,
+        email: "",
+        title: "",
+        subject: "",
+      };
       e.preventDefault();
     },
   },
